07542d926a23d282dbf2ef3f58e784f07104fb4e
wellprog <velorkhalilov@gmail.com>
2018-12-02 18:49:55 +0300

    Добавил базовую модуль

--- TaskManager/Model/ModelBase.cs
+++ TaskManager/Model/ModelBase.cs
@@ -20,6 +20,15 @@ namespace TaskManager.Model
         [Model(IsVisible = false)]
         public bool IsDeleted { get; set; }
 
+        [Model(IsVisible = false)]
+        public Guid UserGuid { get; set; }
+
+        public UserModel GetUser()
+        {
+            //TODO Добавить вывод пользователя по полю UserGuid
+            return null;
+        }
+
         public ModelBase()
         {
             ID = Guid.NewGuid();
---
+++ TaskManager/Model/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Model
+{
+    public class UserModel : ModelBase
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public string nickname { get; set; }
+        public string email { get; set; }
+
+        public UserModel()
+        {
+            login = "";
+            password = "";
+            nickname = "";
+            email = "";
+        }
+    }
+}
