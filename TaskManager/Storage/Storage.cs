8fe34619e5e4efeacebbb82cb700ca8ecbe9358e
shaman9544 <shaman9544@mail.ru>
2018-12-02 18:06:38 +0300

    Store v1

---
+++ TaskManager/Storage/Storage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TaskManager.Storage
+{
+    class Storage: IStorage
+    {
+        public bool Save(Object element)
+        {
+            Type t = element.GetType();
+
+            XmlSerializer formatter = new XmlSerializer(typeof(object));
+
+            using (FileStream fs = new FileStream(t.FullName + ".xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, element);
+            }
+
+            return true;
+        }
+
+        public Object Load(Type element)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(object));
+
+            using (FileStream fs = new FileStream(element.FullName + ".xml", FileMode.OpenOrCreate))
+            {
+                return formatter.Deserialize(fs);
+            }
+        }
+    }
+}
