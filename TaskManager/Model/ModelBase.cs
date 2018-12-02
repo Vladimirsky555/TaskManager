d8f11c7d3fa406fe46e2e005c65d9ec16988cf53
wellprog <velorkhalilov@gmail.com>
2018-12-02 16:01:17 +0300

    Добавил базовый класс модели

---
+++ TaskManager/Model/ModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Model
+{
+    public class ModelBase
+    {
+        public Guid ID { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ModelBase()
+        {
+            ID = Guid.NewGuid();
+            CreateDate = DateTime.Now;
+            UpdateDate = DateTime.Now;
+            IsDeleted = false;
+        }
+    }
+}
