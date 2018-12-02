c97ffb2a72656b3ba7293718cf72a74a086b6a22
Student <Student@STUDOOO-KRGSO10>
2018-12-02 17:40:53 +0300

    данный класс реализует: - регистрацию объектов - разрегистрацию обьектов - возвращает объект по типу - удаляет все объекты из списка - создает объекты

---
+++ TaskManager/Service/ServiceHolder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace TaskManager.Service
+{
+    class ServiceHolder : IServiceHolder
+    {
+       Dictionary<Type, Object> ServiceList;
+       public  bool RegisterService(Type description, Object service)
+        {
+            try
+            {
+                ServiceList.Add(description, service);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool UnRegisterService(Type service)
+        {
+            try
+            {
+                ServiceList.Remove(service);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        public Object GetService(Type service)
+        {
+            return ServiceList[service];
+        }
+
+        public bool ClearAll()
+        {
+            if (ServiceList == null)
+            {
+                return false;
+            }
+            else
+            {
+                ServiceList.Clear();
+                return true;
+            }
+        }
+        public Object CreateObject(Type description)
+        {
+            ConstructorInfo[] ctorInfo = description.GetConstructors();
+            foreach (ConstructorInfo cInfo in ctorInfo)
+            {
+                ParameterInfo[] pInfo = cInfo.GetParameters();
+                List<Object> obj = new List<Object>();
+                foreach (ParameterInfo p in pInfo)
+                {
+                    Object Obj = ServiceList[p.GetType()];
+
+                    if (Obj != null)
+                    {
+                        obj.Add(Obj);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (pInfo.Length == obj.Count)
+                {
+                    return cInfo.Invoke(obj.ToArray());
+                }
+            }
+            return (Object)null;
+        }
+    }
+}
