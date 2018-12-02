269e9ac365c0e97df077411f7de30a5893f3cca8
ziborov.kirill02 <ziborov.kirill02@mail.ru>
2018-12-02 18:15:25 +0300

    Это должно работать-добавлен Float.

---
+++ TaskManager/UIFragment/UserControl2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace TaskManager.UIFragment
+{
+    public partial class UserControl2 : UserControl, IUIPart
+    {
+        Object model;
+        PropertyInfo fieldInfo;
+        public UserControl2()
+            {
+            InitializeComponent();
+            }
+        public void SetLabel(string label)
+        {
+            this.label2.Text = label;
+        }
+        public void SetData(Object model, PropertyInfo fieldInfo)
+        {
+            this.model = model;
+            this.fieldInfo = fieldInfo;
+            this.textBox2.Text = fieldInfo.GetValue(model).ToString();
+        }
+        public bool Validate()
+        {
+            if(this.textBox2.Text == "")
+            {
+                return false;
+            }
+            float value;
+            bool chek = float.TryParse(this.textBox2.Text, out value);
+            return chek;
+        }
+        public void Save()
+        {
+            float value;
+            bool chek = float.TryParse(this.textBox2.Text,out value);
+
+            fieldInfo.SetValue(model, value);
+        }
+       
+    }
+}
